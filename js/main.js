202db2e3ba6347ed72e375069a365ade8bfcb6c3
Gilbert Fung <gilbertfung@outlook.com>
2016-06-27 22:13:38 -0700

    Add add-to-cart interaction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+	// ===== Main navigation ===== //
 
+	// Cart interaction
+
+	// ===== Product pages ===== //
+	// Add to cart interaction
+	$('.js-add-to-cart').click(function(event){
+		event.preventDefault();
+		$(this).text('Added to cart!');
+		$('.header-nav .icon-cart .badge').removeClass('hidden');
+		$('.header-nav .icon-cart .badge').addClass('shown');
+	});
 });
